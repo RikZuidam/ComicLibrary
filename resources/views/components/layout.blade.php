e8e9b3a0c61f3df397e8fc1248b0bed91414563b
RikZuidam <rhj.zuidam@gmail.com>
2022-11-18 11:17:15 +0100

    test

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- Icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
+    <script src="//unpkg.com/alpinejs" defer></script>
+    <title>Document</title>
+</head>
+<body>
+    <nav></nav>
+    <main>
+        {{ $slot }}
+    </main>
+    <footer></footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome3');
+    return view('home');
 });
 
-Route::get('/', function () {
-    return view('welcome1');
-});
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
 
+Route::post('/loginSubmit', [UserController::class, 'login']);
