63cc8dec3544ac59eaa5c3d8c60fc09e63078597
Rosux <jcsbt2015@gmail.com>
2022-11-22 14:24:04 +0100

    test

---
+++ public/cart.js
@@ -0,0 +1,17 @@
+class cart{
+    constructor(){
+        this.cart = [];
+    }
+    /**
+     * gets the shoping cart items from the database
+     */
+    getCart(){
+
+    }
+    /**
+     * saves the cart to cookies/localstorage
+     */
+    saveCart(){
+
+    }
+}
