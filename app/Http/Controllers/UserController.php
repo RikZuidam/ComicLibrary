634e65828d3d19b255197ad1483a1280340238b3
RikZuidam <rhj.zuidam@gmail.com>
2022-11-22 16:15:18 +0100

    ready for testing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,13 +19,34 @@ class UserController extends Controller
     public function register()
     {
         return view('user.register');
-        //
     }
 
     public function create(Request $request)
     {
-        //
-        // Auth
+        // Validate Formfields
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('authenticate', 'email')],
+            'password' => 'required|confirmed|min:3'
+        ]);
+
+        // Hash Password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        // Create User
+        $user = User::create([
+            'name' => $formFields['name'],
+            'email' => $formFields['email'],
+            'verify_token' => 'inprogress',
+            'email_verified_at' => null,
+            'password' => $formFields['password']
+        ]);
+
+        // Authentication add login statement
+        auth()->login($user);
+
+        // Redirect to Home-Page
+        return redirect('/')->with('message', 'Welcome ' . $formFields['name']);
     }
 
     public function login()
