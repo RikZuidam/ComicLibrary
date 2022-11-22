04eff7ddd7268654752bbdca71c7970856c34e6a
RikZuidam <rhj.zuidam@gmail.com>
2022-11-22 16:19:14 +0100

    role default 0

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('verify_token')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('role');
+            $table->integer('role')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
