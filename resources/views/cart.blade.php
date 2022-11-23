5928c593260581a26bffd1325b1c1a659d328b23
Rosux <jcsbt2015@gmail.com>
2022-11-23 11:57:06 +0100

    yeet

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
     <h1>CART PAGE</h1>
+    <p add-to-cart="12">ADD TO CART 12</p>
+    <p add-to-cart="13">ADD TO CART 13</p>
+    <p add-to-cart="14">ADD TO CART 14</p>
+    <p add-to-cart="15">ADD TO CART 15</p>
 </x-layout>
