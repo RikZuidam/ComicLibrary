89d00f5a1c8c34270931fd4ba4ee87cf18485ce2
Rosux <jcsbt2015@gmail.com>
2022-11-23 22:06:06 +0100

    cart finalized + header

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,7 +1,3 @@
 <x-layout>
-    <h1>CART PAGE</h1>
-    <p add-to-cart="12">ADD TO CART 12</p>
-    <p add-to-cart="13">ADD TO CART 13</p>
-    <p add-to-cart="14">ADD TO CART 14</p>
-    <p add-to-cart="15">ADD TO CART 15</p>
+Cart
 </x-layout>
