58e5ea309802562fc6bffe356ced4a82be0b540b
RikZuidam <rhj.zuidam@gmail.com>
2022-11-23 08:59:54 +0100

    scripts

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -54,7 +54,7 @@ class ShoppingCart{
      * updates the DOM cart counter number
      */
     updateCartCount(){
-        cartCount = document.querySelector(".cart-overlay > .cart-count");
+        const cartCount = document.querySelector(".cart-overlay > .cart-count");
         this.cart.length > 9 ? cartCount.innerText = "9+" : cartCount.innerText = this.cart.length;
     }
     #setCookie(name, value, days=30){
@@ -83,10 +83,10 @@ class ShoppingCart{
      * Adds the cart overlay button to the body
      */
     addCartOverlay(){
-        cartWrapper = document.createElement("a");
+        let cartWrapper = document.createElement("a");
         cartWrapper.setAttribute("href", "/cart");
         cartWrapper.classList.add("cart-overlay");
-        cartItemCount = document.createElement("p");
+        let cartItemCount = document.createElement("p");
         cartItemCount.classList.add("cart-count");
         cartItemCount.innerText = 0;
         cartWrapper.innerHTML = `<svg viewBox="0 0 24 21"><path d="M24 3l-.743 2h-1.929l-3.474 12h-13.239l-4.615-11h16.812l-.564 2h-13.24l2.937 7h10.428l3.432-12h4.195zm-15.5 15c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.9-7-1.9 7c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>`;
