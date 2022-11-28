c94c0bf9cb8834af627274eec3fe8230cdf38b20
Rosux <jcsbt2015@gmail.com>
2022-11-28 10:47:56 +0100

    banner

---
+++ resources/views/partials/banner.blade.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="{{ asset('/assets/css/banner.css') }}">
+<div class="banner-full-width">
+    <div class="banner-darken-image">
+        <div class="banner-title-small"><p>ComicLibrary</p></div>
+        <div class="banner-title"><p>YOUR COMIC BOOKS ONLINE!</p></div>
+    </div>
+</div>
